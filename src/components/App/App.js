8f06e6636a3513223ce033980ad0e631af54eb8e
bryce <brycemorries@gmail.com>
2023-05-12 10:25:17 -0500

    merging

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import SearchGiphy from '../SearchGiphy/SearchGiphy';
 import DisplaySearchedGifs from '../DisplaySearchedGifs/DisplaySearchedGifs';
-
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 function App(props) {
   return (
     <div>
       <h1>Giphy Search!</h1>
-      <SearchGiphy />
-      <DisplaySearchedGifs />
+
+      <Router>
+        <Route exact path='/'>
+          <SearchGiphy />
+          <DisplaySearchedGifs />
+        </Route>
+        <Route exact path='/favorite'>
+          <FavoritePage />
+        </Route>
+      </Router>
     </div>
   );
 }
--- src/components/SearchGiphy/SearchGiphy.jsx
+++ src/components/SearchGiphy/SearchGiphy.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useDispatch } from 'react-redux';
+import './SearchGiphy.css';
 
 function SearchGiphy() {
 
@@ -22,6 +23,11 @@ function SearchGiphy() {
       }
     })
   }
+  const displayFavorite = () => {
+    dispatch({
+      type:''
+    })
+  }
 
   const handleRatingChoice = event => {
     setRating(event.target.value)
@@ -33,50 +39,61 @@ function SearchGiphy() {
 
   return (
     <div>
-    <header className="App-header">
-      <h1>Search Giphy API</h1>
-    </header>
-    <div>
-    <h3>Choose a rating for the Gifs:</h3>
-      <input 
-        type='radio'
-        value='g'
-        name='rating'
-        onChange={handleRatingChoice}
-      />
-      <label htmlFor='g'>G</label>
-      <input
-        type='radio' 
-        value='pg' 
-        name='rating'
-        onChange={handleRatingChoice}
-      />
-      <label htmlFor='pg'>PG</label>
-      <input
-        type='radio'
-        value='pg-13'
-        name='rating'
-        onChange={handleRatingChoice}
-      />
-      <label htmlFor='pg-13'>PG-13</label>
-      <input
-        type='radio'
-        value='r'
-        name='rating'
-        onChange={handleRatingChoice}
-      />
-      <label htmlFor='r'>R</label>
-
-      <h3>Enter a keyword for the Gifs:</h3>
-        <input 
+      <header className="App-header">
+        <h1>Search Giphy API</h1>
+      </header>
+      <div>
+        <h3>Choose a rating for the Gifs:</h3>
+        <input
+          type='radio'
+          value='g'
+          name='rating'
+          onChange={handleRatingChoice}
+        />
+        <label htmlFor='g'>G</label>
+        <input
+          type='radio'
+          value='pg'
+          name='rating'
+          onChange={handleRatingChoice}
+        />
+        <label htmlFor='pg'>PG</label>
+        <input
+          type='radio'
+          value='pg-13'
+          name='rating'
+          onChange={handleRatingChoice}
+        />
+        <label htmlFor='pg-13'>PG-13</label>
+        <input
+          type='radio'
+          value='r'
+          name='rating'
+          onChange={handleRatingChoice}
+        />
+        <label htmlFor='r'>R</label>
+          <h3>Enter a keyword for the Gifs:</h3>
+        <input
           type='text'
           onChange={handleSearchEntry}
           value={search}
         />
-      <br />
-      <button onClick={searchGIFS}>Search Gifs</button>
+        <br />
+        <button onClick={searchGIFS}>Search Gifs</button>
+        <br />
+        <br />
+        <div className="fav-cat">
+          <select name="Favorites" id="favorites" onchange={favoritePgae}>
+            <option selected disabled hidden>Favorites</option>
+            <option value="cartoon">Cartoon</option>
+            <option value="cohort">Cohort</option>
+            <option value="funny">Funny</option>
+            <option value="meme">Meme</option>
+            <option value="nsfw">Nsfw</option>
+          </select>
+        </div>
+      </div>
     </div>
-  </div>
   )
 }
 
